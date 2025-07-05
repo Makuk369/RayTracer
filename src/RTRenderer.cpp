dde8f00bed7f715bad539b193849565fdaec1dfd
Makuk369 <makuk369@gmail.com>
2025-07-05 12:46:14 +0200

    ep 3 working

--- src/RTRenderer.cpp
+++ src/RTRenderer.cpp
@@ -1,7 +1,11 @@
 #include "headers/RTRenderer.hpp"
 
+// RTRenderer::RTRenderer(){
+
+// }
+
 void RTRenderer::Render(SDL_Surface* surface){
-	SDL_LockSurface(surface);
+    SDL_LockSurface(surface);
 	SDL_PixelFormat pixelFormat = surface->format;
 	const SDL_PixelFormatDetails *pixelFormatDetails = SDL_GetPixelFormatDetails(pixelFormat);
 	uint8_t* pixelArray = (uint8_t*)surface->pixels;
@@ -10,8 +14,8 @@ void RTRenderer::Render(SDL_Surface* surface){
 	for (int y = 0; y < surface->h; y++){
 		for (int x = 0; x < surface->w; x++){
 
-			glm::vec2 coord = {x / (float)surface->w, 1 - y / (float)surface->h}; // 0 - surface size -> 0 - 1
-			// coord = coord * 2.0f - 1.0f; // 0 - 1 -> -1 - 1
+			glm::vec2 coord(x / (float)surface->w, 1 - y / (float)surface->h); // 0 - surface size -> 0 - 1
+			coord = coord * 2.0f - 1.0f; // 0 - 1 -> -1 - 1
 
 			// ----- setting pixels -----
 			Uint32 * const target_pixel = (Uint32 *) ((Uint8 *) surface->pixels + y * surface->pitch + x*pixelFormatDetails->bytes_per_pixel);
@@ -26,7 +30,9 @@ void RTRenderer::Render(SDL_Surface* surface){
 // returns color of set pixel (format = ARGB 0xff000000)
 uint32_t RTRenderer::PerPixel(glm::vec2 coord){
 
-	// glm::vec3 rayDirection = {coord.x, coord.y, -1.0f};
+	glm::vec3 rayOrigin(0.0f, 0.0f, 2.0f);
+	glm::vec3 rayDirection(coord.x, coord.y, -1.0f);
+	float radius = 0.5f;
 
 	// (bx^2 + by^2)t^2 + (2(axbx + ayby))t + (ax^2 + ay^2 - r^2) = 0
 	// where
@@ -34,8 +40,16 @@ uint32_t RTRenderer::PerPixel(glm::vec2 coord){
 	// b = ray direction
 	// r = radius
 	// t = hit distance
+	float a = glm::dot(rayDirection, rayDirection);
+	float b = 2.0f * glm::dot(rayOrigin, rayDirection);
+	float c = glm::dot(rayOrigin, rayOrigin) - radius * radius;
 
-	uint8_t r = (uint8_t)(coord.x * 255.0f);
-	uint8_t g = (uint8_t)(coord.y * 255.0f);
-    return 0xff000000 | (r << 16) | (g << 8);
+	// Quadratic forumula discriminant:
+	// b^2 - 4ac
+	float discriminant = b * b - 4.0f * a * c;
+	if (discriminant >= 0.0f){
+		return 0xffff00ff;
+	}
+
+    return 0xff000000;
 }
--- src/headers/GameSettings.hpp
+++ src/headers/GameSettings.hpp
@@ -5,6 +5,6 @@ namespace RayTracerSetings{
     inline const bool SHOW_FPS = true;
     inline const uint16_t MAX_FPS = 60;
 
-    inline const uint16_t WINDOW_WIDTH = 1000;
-    inline const uint16_t WINDOW_HEIGHT = 1000;
+    inline const uint16_t WINDOW_WIDTH = 800;
+    inline const uint16_t WINDOW_HEIGHT = 800;
 }
