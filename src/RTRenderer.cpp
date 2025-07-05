e4fcc0e5e91e26acf93e3b5cbb08d51ef3a31a5c
Makuk369 <makuk369@gmail.com>
2025-07-05 10:39:55 +0200

    most basic shader working

--- src/RTRenderer.cpp
+++ src/RTRenderer.cpp
@@ -1,5 +1,5 @@
 #include "headers/RTRenderer.hpp"
-#include "headers/Structures.hpp"
+#include "RTRenderer.hpp"
 
 void RTRenderer::Render(SDL_Surface* surface){
 	SDL_LockSurface(surface);
@@ -7,23 +7,25 @@ void RTRenderer::Render(SDL_Surface* surface){
 	const SDL_PixelFormatDetails *pixelFormatDetails = SDL_GetPixelFormatDetails(pixelFormat);
 	uint8_t* pixelArray = (uint8_t*)surface->pixels;
 
-	uint32_t color = 0xff000000; //ARGB
-
 	// looping through every pixel
 	for (int y = 0; y < surface->h; y++){
 		for (int x = 0; x < surface->w; x++){
 
-			Vec2 coord = {x / (float)surface->w, y / (float)surface->h};
+			Vec2 coord = {x / (float)surface->w, y / (float)surface->h}; // 0-surface size -> 0-1
 
-			color = SDL_rand_bits();
-			color |= 0xff000000;
-
-			// ----- setting pixel color -----
+			// ----- setting pixels -----
 			Uint32 * const target_pixel = (Uint32 *) ((Uint8 *) surface->pixels + y * surface->pitch + x*pixelFormatDetails->bytes_per_pixel);
-  			*target_pixel = color;
-			// -------------------------------
+  			*target_pixel = PerPixel(coord);
+			// --------------------------
 		}
 	}
 
 	SDL_UnlockSurface(surface);
+}
+
+// returns color of set pixel (format = ARGB 0xff000000)
+uint32_t RTRenderer::PerPixel(Vec2 coord){
+	uint8_t r = (uint8_t)(coord.x * 255.0f);
+	uint8_t g = (uint8_t)(coord.y * 255.0f);
+    return 0xff000000 | (r << 16) | (g << 8);
 }
--- src/headers/RTRenderer.hpp
+++ src/headers/RTRenderer.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <SDL3/SDL.h>
+#include "Structures.hpp"
 
 class RTRenderer{
     public:
@@ -8,4 +9,5 @@ class RTRenderer{
         void Render(SDL_Surface* surface);
 
     private:
+        uint32_t PerPixel(Vec2 coord);
 };
